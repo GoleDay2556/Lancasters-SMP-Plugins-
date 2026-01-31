95d2550851c9132193e982f161728ac79d45bb5f
GoleDay2556 <golovanov.dmitrii@outlook.com>
2026-01-31 17:15:55 +0200

    Patch V1.1Web

--- Server Status/Web_Server_Status/WebStatus.py
+++ Server Status/Web_Server_Status/WebStatus.py
@@ -5,7 +5,7 @@ import json
 import sys
 import os
 
-VERSION = "V1.0Web" #Version (DO NOT CHANGE)
+VERSION = "V1.1Web" #Version (DO NOT CHANGE)
 CONFIG_FILE = "config.json" #Configuration File (DO NOT CHANGE)
 
 def load_config():
@@ -22,8 +22,8 @@ STATUSPAGE_API_KEY = config["statuspage_api_key"]
 PAGE_ID = config["page_id"]
 COMPONENT_ID = config["component_id"]
 
-WEB_HOST = config["panel_host"]
-WEB_PORT = config.get("panel_port", 25565)
+WEB_HOST = config["web_host"]
+WEB_PORT = config.get("web_port", 25565)
 CHECK_INTERVAL = config.get("check_interval", 60)
 
 def is_server_online():
